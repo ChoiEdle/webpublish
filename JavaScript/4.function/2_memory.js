7c9aee8d0b28436ce12af46355ed05797fa7c89b
choi <cjy1749@naver.com>
2025-08-20 14:40:52 +0900

    08-20 commit

---
+++ JavaScript/4.function/2_memory.js
@@ -0,0 +1,38 @@
+/**
+ * 함수 생성 및 호출
+ */
+
+//문자 --> 숫자 변환 함수
+function toNumber(num1, num2) {
+    let obj = {
+        'num1' : parseInt(num1),
+        'num2' : parseInt(num2)
+    }
+    return obj;
+}
+
+add(100, 200);
+add('100', '200');
+
+function add(num1, num2) {
+    let obj = toNumber(num1, num2);
+    console.log(`${obj.num1} + ${obj.num2} = ${obj.num1+obj.num2}`);
+}
+
+//Arrow funtion
+const min = (num1, num2) => {
+    let obj = toNumber(num1, num2);
+    // if(typeof num1 === 'string' && typeof num2 === 'string'){
+    //     num1 = parseInt(num1);
+    //     num2 = parseInt(num2);
+    // }
+    if(obj.num1>=obj.num2){
+        console.log(`${obj.num1} - ${obj.num2} = ${obj.num1-obj.num2}`);
+    } else {
+        console.log(`${obj.num2} - ${obj.num1} = ${obj.num2-obj.num1}`);
+    }
+}
+
+min(200, 100);
+min('200','100');
+
