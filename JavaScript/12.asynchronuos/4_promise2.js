e0e4bec3280dd684dea6f4a3fa6b6517b580d579
choi <cjy1749@naver.com>
2025-09-03 12:46:44 +0900

    09-03 commit

---
+++ JavaScript/12.asynchronuos/4_promise2.js
@@ -0,0 +1,25 @@
+/**
+ * Pizza 주문 처리 로직
+ * - 1. 주문
+ * - 2. 피자 만들기 : 5초 걸림
+ * - 3. 배달
+ */
+
+async function pizza() {
+    let makePizza = new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            // console.log(`2. 피자 완성`);
+            resolve();
+        }, 2000);
+    });
+    
+    console.log(`1. 주문 완료`);
+    
+    await makePizza.then((resolve) => {console.log(`2. 피자 완성`)}).catch((reject) => {console.log(`--> reject`);});
+
+    console.log(`3. 배달 완료`);
+
+}
+
+pizza();
+
