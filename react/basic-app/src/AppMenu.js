ddff1f468a6193bbd1e491de68a891f52934a20d
choi <cjy1749@naver.com>
2025-09-12 12:45:39 +0900

    09-12 commit

--- react/basic-app/src/AppMenu.js
+++ react/basic-app/src/AppMenu.js
@@ -1,16 +1,34 @@
 import "./css/Menu.css";
-import { Menu } from "./components/commons/Menu.jsx";
-import { HeaderLeft } from "./components/menus/HeaderLeft.jsx";
+import { MenuList } from "./components/menus/MenuList.jsx";
+import { useState, useEffect } from "react";
+import { fetchData } from "./util/commonData.js"
+import { Logo } from "./components/commons/Logo.jsx";
+
 
 export default function App() {
+    const [menus, setMenus] = useState({});
+    
+    useEffect(() => {
+        const fetch = async() => {
+            const data = await fetchData("/data/menus.json");
+            setMenus(data);
+        }
+        fetch();
+    }, [])
+
     return (
-        <>
-            <HeaderLeft />
-            <div style={{display:"flex"}}>
-                <Menu href="#" name="Menu1" isIcon="true" icon="🚨" style={{"w":"100px", "h":"50px", "bg":"#777", "color":"#fff"}} />
-                <Menu href="#" name="Menu2" style={{"w":"100px", "h":"50px", "bg":"#777", "color":"#fff"}} />
-                <Menu href="#" name="Menu3" style={{"w":"100px", "h":"50px", "bg":"#777", "color":"#fff"}} />
+        <div>
+            <div style={{display:"flex", justifyContent:"space-between", borderBottom: "1px solid #888", paddingBottom: "20px"}}>
+                <MenuList menus={menus.headerLeft} />
+                <Logo img="/images/logoRed.png" alt="header-logo" w="120px"/>
+                <MenuList menus={menus.headerRight} />
             </div>
-        </>
+            <div style={{display:"flex", justifyContent:"center"}}>
+                <MenuList menus={menus.headerCenter} />
+            </div>
+            <div style={{display:"flex", justifyContent:"center"}}>
+                <MenuList menus={menus.footer} />
+            </div>
+        </div>
     );
 }
