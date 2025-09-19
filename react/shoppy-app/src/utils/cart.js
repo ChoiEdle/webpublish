f33b4aa9b465ae97f8f6ffc6dee5ff3bfd562bd7
choi <cjy1749@naver.com>
2025-09-19 16:27:21 +0900

    09-19 commit

--- react/shoppy-app/src/utils/cart.js
+++ react/shoppy-app/src/utils/cart.js
@@ -1,15 +1,12 @@
 
 export function cartItemsCheck(prevItems, cartItem) {
     //존재여부 체크
-    const existItem = prevItems.find((item) => 
-    item.pid === cartItem.pid && item.size === cartItem.size);
+    const existItem = prevItems.find((item) => item.pid === cartItem.pid && item.size === cartItem.size);
 
     if(existItem) {   //존재하면 map으로 순회하면서 pid, size가 동일한 item에 qty +1
-    return prevItems.map((item) => 
-        item.pid === cartItem.pid && item.size === cartItem.size ? {...item, qty:item.qty+1} : item
-    );
+        return prevItems.map((item) => item.pid === cartItem.pid && item.size === cartItem.size ? {...item, qty:item.qty+1} : item);
     } else {
-    return [...prevItems, {...cartItem}];   //존재하지 않으면 새로운 item 추가
+        return [...prevItems, {...cartItem}];   //존재하지 않으면 새로운 item 추가
     }
     
 }
