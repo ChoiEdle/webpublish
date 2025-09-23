2a75f3d5bd803932990ec82e796a390f460e8355
choi <cjy1749@naver.com>
2025-09-23 12:48:43 +0900

    09-23 commit

--- react/shoppy-app/src/pages/Cart.jsx
+++ react/shoppy-app/src/pages/Cart.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { axiosData } from '../utils/dataFetch.js';
+import { cartItemsAddInfo } from '../utils/cart.js';
+import '../styles/cart.css';
 
-export function Cart() {
+export function Cart({items}) {
+    const [cartList, setCartList] = useState(items);
+    useEffect(() => {
+        const load = async() => {
+            const jsonData = await axiosData("/data/products.json");
+            setCartList(cartItemsAddInfo(jsonData, items));
+        }
+        load();
+    }, []);
+
+    const handleUpdateCartList = (cid, type) => {
+        setCartList((cartList)=>
+            cartList.map(item => 
+                item.cid === cid ? type === '+' ? {...item, qty: item.qty + 1 } : {...item, qty: item.qty - 1 } : item
+            )
+        );
+        // type==="-" ? setCartList(cartList.map(item => item.cid === cid ? {...item, qty:(item.qty>1 ? item.qty-1 : 1)} : item)) : setCartList(cartList.map(item => item.cid === cid ? {...item, qty:item.qty+1} : item))
+    }
+    
     return (
-        <div>
-            <h1>My Cart!!</h1>
+        <div className='cart-container'>
+            <h2 className='cart-header'>장바구니</h2>
+            {
+                cartList && cartList.map(item => 
+                    <div>
+                        <div className='cart-item'>
+                            {item.cid}
+                            <img src={item.image} alt='product img' />
+                            <div className='cart-item-details'>
+                                <p className='cart-item-title'>{item.name}</p>
+                                <p className='cart-item-title'>{item.size}</p>
+                                <p className='cart-item-title'>{parseInt(item.price).toLocaleString()}원</p>
+                            </div>
+                            <div className='cart-quantity'>
+                                <button type='button' onClick={()=>{handleUpdateCartList(item.cid, "-")}}>-</button>
+                                <input type="text" value={item.qty} readOnly/>
+                                <button type='button' onClick={()=>{handleUpdateCartList(item.cid, "+")}}>+</button>
+                            </div>
+                            <button className='cart-remove'>
+                                <RiDeleteBin6Line />
+                            </button>
+                        </div>
+
+                    </div>
+                )
+            }
         </div>
     );
 }
--- react/shoppy-app/src/utils/cart.js
+++ react/shoppy-app/src/utils/cart.js
@@ -1,4 +1,16 @@
+/**
+ * 상품리스트에서 이미지, 상품명, 가격 --> 장바구니 리스트에 추가
+ */
+export function cartItemsAddInfo(products, items) {
+    return items.map((item) => {
+        const product = products.find((product) => item.pid === product.pid);
+        return {...item, image:product.image, name:product.name, price:product.price}
+    });
+}
 
+/**
+ * 장바구니 수량 증가 체크 함수
+ */
 export function cartItemsCheck(prevItems, cartItem) {
     //존재여부 체크
     const existItem = prevItems.find((item) => item.pid === cartItem.pid && item.size === cartItem.size);
@@ -6,7 +18,7 @@ export function cartItemsCheck(prevItems, cartItem) {
     if(existItem) {   //존재하면 map으로 순회하면서 pid, size가 동일한 item에 qty +1
         return prevItems.map((item) => item.pid === cartItem.pid && item.size === cartItem.size ? {...item, qty:item.qty+1} : item);
     } else {
-        return [...prevItems, {...cartItem}];   //존재하지 않으면 새로운 item 추가
-    }
-    
+        const cid = Math.floor(Math.random()*10000000);
+        return [...prevItems, {...cartItem, cid:cid }];   //존재하지 않으면 새로운 item 추가
+    }    
 }
